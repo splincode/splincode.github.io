e55f1bcd472043dd2025ec93a24ffab7bac836dc
splincode <omaxphp@yandex.ru>
2016-04-12 21:47:49 +0300

    update

--- view/maker/js/app.js
+++ view/maker/js/app.js
@@ -1,18 +1,11 @@
 $(function(){
 
-	$.ajaxPrefilter( function (options) {
-	  if (options.crossDomain && jQuery.support.cors) {
-	    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-	    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-	    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+	$.ajax({
+	  url:"http://www.ontvtime.ru/tv/?cat=10&mode=0&dates0=2016-04-11&dates1=2016-04-12&dates2=2016-04-13&dates3=2016-04-14&dates4=2016-04-15&dates5=2016-04-16&dates6=2016-04-17&ch0=159",
+	  dataType: "jsonp",
+	  success: function (data) {
+	    alert(data);
 	  }
-	});
-
-	$.get(
-	    'http://new.s-tv.ru/tv/',
-	    function (response) {
-	        console.log("> ", response);
-	       // $("#viewer").html(response);
 	});
 
 
