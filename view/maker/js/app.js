a98512cdebd6bf661ff22783262dce987edbc78f
splincode <omaxphp@yandex.ru>
2016-04-12 20:39:11 +0300

    update

--- view/maker/js/app.js
+++ view/maker/js/app.js
@@ -2,20 +2,11 @@ $(function(){
 
 	$.ajax({
 	  url:"http://new.s-tv.ru/tv/?callback=true",
-	  dataType: "jsonp",
+	  dataType: "text/plain",
 	  success: function (data) {
-	    console.log(data)
 	    alert(data);
 	  }
 	});
 
-	$('iframe').load(function(){
-		var iframe = $(this).contents();
-		
-		//iframe.find("#main").children("#header").remove();
-		console.log(iframe)
-
-
-	});
 
 });
