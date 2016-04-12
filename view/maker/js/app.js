8d746fb078bc7ba2f7845479352b0b31c3cefcd6
splincode <omaxphp@yandex.ru>
2016-04-12 20:40:19 +0300

    update

--- view/maker/js/app.js
+++ view/maker/js/app.js
@@ -1,12 +1,20 @@
 $(function(){
 
-	$.ajax({
-	  url:"http://new.s-tv.ru/tv/?callback=true",
-	  dataType: "text/plain",
-	  success: function (data) {
-	    alert(data);
+	$.ajaxPrefilter( function (options) {
+	  if (options.crossDomain && jQuery.support.cors) {
+	    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+	    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+	    //options.url = "http://cors.corsproxy.io/url=" + options.url;
 	  }
 	});
+
+	$.get(
+	    'http://new.s-tv.ru/tv/',
+	    function (response) {
+	        console.log("> ", response);
+	       // $("#viewer").html(response);
+	});
+
 
 
 });
