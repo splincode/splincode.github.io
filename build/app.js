583118ca74480f476c8c2f4fa52a251c5d910ff6
splincode <omaxphp@yandex.ru>
2016-06-28 02:36:43 +0300

    update

--- build/app.js
+++ build/app.js
@@ -1,67 +1,57 @@
+var count = 37,
+  			section = Math.ceil( count / 15 ),
+  			dom = '',
+  			k = 1;
+  		
 
-	alert("после локала")
+  		for (var j = 1; j <= section; j++){
 
+  			dom += "<div class='frameSlider'>";
+  		
+  			for(var i=k; i<(k+15); i++){
+  				
+  				dom += 
+  				"<image class='img' " +
+  				"src='https://raw.githubusercontent.com/splincode/splincode.github.io/master/portfolio/preview/" + i +".jpg'/>";
+  				
+  				count--;
+  				if (count == 0) break;
+  			}
+  		
+  			k += 15;
+  			dom += `</div>`;
+  		}
 
-		$(document).ready(function() {		
-			//$('body').addClass("fadeIn");
+  		document.getElementById("innerSlider").innerHTML = dom;
+
+		var slider = new Spliny();
+
+		$('.right_button').on("click", function(){
+			slider.right();
 		});
-	
-		// заполняем кадры изображениями
-		var count = 37;
-		var section = Math.ceil( count / 15 );
-		var dom = '';
 
-		var k = 1;
-		for (var j = 1; j <= section; j++){
-			dom += "<div class='frameSlider'>";
-	
-			for(var i=k; i<(k+15); i++){
-				dom += 
-					"<image "  +
-						"class='img' " +
-						"src='https://raw.githubusercontent.com/splincode/splincode.github.io/master/portfolio/preview/" + i +".jpg' " +
-					"/>\n";
-				
-				count--;
-				if (count == 0) break;
-			}
-		
-			k += 15;
-			dom += `</div>`;
+		$('.left_button').on("click", function(){
+			slider.left();
+		});
+
+
+		var w = $(window).width();
+		if (w > 640) {
+
+			var video = $("<video width='6000px' height='6000px' loop autoplay>" +
+			  "<source src='build/media/intro.mp4' type='video/mp4'>" +
+			" </video>");
+
+			$('.layer').before(video);
+
+			video.bind("canplaythrough", function(){
+				$('video').addClass("fadeInVideo")
+			    $('.layer').css('opacity', '0.7');
+			});
+
 		}
-		//alert(dom)
-		$("#innerSlider").html(dom);
+	
 
-		$(window).on('load', function(){
-    		
-    		// слайдер 
-    		var slider = new Spliny();
-
-    		$('.right_button').on("click", function(){
-    			slider.right();
-    		});
-
-    		$('.left_button').on("click", function(){
-    			slider.left();
-    		});
-
-    		// видео
-    		var w = $(window).width();
-			if (w > 640) {
-
-				var path = 'build/media/intro.mp4';
-				var video = 
-				$("<video width='6000px' height='6000px' loop autoplay>" +
-				  "<source src='" + path + "' type='video/mp4'>" +
-				" </video>");
-
-				$('.layer').before(video);
-
-	 			video.bind("canplaythrough", function(){
-	 				$('video').addClass("fadeInVideo")
-				    $('.layer').css('opacity', '0.7');
-				});
-
-			}
-
-    	});
+  		$(document).ready(function() {		
+			$('body').addClass("fadeIn");
+		});
