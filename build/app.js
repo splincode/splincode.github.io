83fba927669175f48b249093fd7da02940bdaeaf
splincode <omaxphp@yandex.ru>
2016-07-03 18:25:41 +0300

    update

--- build/app.js
+++ build/app.js
@@ -171,8 +171,8 @@ var Spliny = function(){
         "src='https://raw.githubusercontent.com/splincode/splincode.github.io/master/portfolio/origin/" + i +".jpg'/>";
           
       document.getElementById("baskmg").innerHTML = dom;
-      alert(arrayText[i+1])
-      $('.textmg').text(arrayText[i+1]);
+
+      $('.textmg').text(arrayText[i-1]);
       $(".modal").css('display', 'block').attr("data-active", "true");
 
     });
